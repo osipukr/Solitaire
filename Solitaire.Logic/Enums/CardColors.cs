c4287b82daea5e9d5f518523ff5b12eaf108e9b0
osipukr <fredstone624@gmail.com>
2020-06-30 18:09:58 +0300

    Initial commit

---
+++ Solitaire.Logic/Enums/CardColors.cs
@@ -0,0 +1,18 @@
+﻿namespace Solitaire.Logic.Enums
+{
+    /// <summary>
+    /// Represents a card colors.
+    /// </summary>
+    public enum CardColors
+    {
+        /// <summary>
+        /// Black cards (spades or clubs).
+        /// </summary>
+        Black,
+
+        /// <summary>
+        /// Red cards (hearts or diamonds). 
+        /// </summary>
+        Red
+    }
+}
---
+++ Solitaire.Logic/Enums/CardSuits.cs
@@ -0,0 +1,28 @@
+﻿namespace Solitaire.Logic.Enums
+{
+    /// <summary>
+    /// Represents a card's suit.
+    /// </summary>
+    public enum CardSuits
+    {
+        /// <summary>
+        /// Hearts. 
+        /// </summary>
+        Hearts,
+
+        /// <summary>
+        /// Diamonds.
+        /// </summary>
+        Diamonds,
+
+        /// <summary>
+        /// Clubs.
+        /// </summary>
+        Clubs,
+
+        /// <summary>
+        /// Spades.
+        /// </summary>
+        Spades
+    }
+}
